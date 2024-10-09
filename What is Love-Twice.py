412b4b21a4c8b71d97c33dfb1c80ff12f0983034
Angel Aurelia <115779785+vcnillatte@users.noreply.github.com>
2024-10-09 07:40:54 +0000

    Pending changes exported from your codespace

---
+++ What is Love-Twice.py
@@ -0,0 +1,27 @@
+import sys
+from time import sleep
+import time
+
+def print_lyrics():
+    lines = [
+        ("I'mma get it", 0.098),
+        ("done" , 0.1),
+        ("I'm falling in love again", 0.1),
+        ("Oh", 0.2),
+        ("I'm falling in love", 0.1),
+        ("I thought the plane was going down", 0.08),
+        ("How'd you turn it right around?", 0.1),
+    ]
+
+    delays = [1, 0.3, 0.5, 1, 0.2, 0.01, 0.1]
+
+    for i, (line, char_delay) in enumerate(lines):
+        for char in line:
+            print(char, end='')
+            sys.stdout.flush()
+            sleep(char_delay)
+        time.sleep(delays[i])
+        print('')
+    
+print_lyrics()
+    
